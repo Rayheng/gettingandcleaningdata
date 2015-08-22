0c5287d7aabd445478dc88c08dc8de519dd21b27
Ray Heng <hengrj@gmail.com>
2015-08-23 04:51:48 +0800

    Release 1.0

--- readme.md
+++ readme.md
@@ -1,22 +1,76 @@
 ---
-title: "run_analysis_Cookbook"
-author: "Heng Rui Jie"
-date: "22 August, 2015"
-output: html_document
+Title: "run_analysis_Cookbook"
+Author: "Heng Rui Jie"
+Date: "22 August, 2015"
+Output: html_document
 ---
 
-This is an R Markdown document. Markdown is a simple formatting syntax for authoring HTML, PDF, and MS Word documents. For more details on using R Markdown see <http://rmarkdown.rstudio.com>.
+1. Purposes of the Readme Markdown file:
+- Provides the overview of the key challenges faced and assumptions made in the course of producing the final tidy data
+- Gives an overall approach in getting and cleaning the data for run_analysis.R
+- Describes the labels/variable names in the final tidy data for the user to use and explore the dataset further
 
-When you click the **Knit** button a document will be generated that includes both content as well as the output of any embedded R code chunks within the document. You can embed an R code chunk like this:
+Note: run_analysis.R contains the comments that depict the various functions used throughout the R script. Reader may refer to the script itself for more details on the producing the final tidy data
 
-```{r}
-summary(cars)
-```
+2. Getting and Cleaning Data Approach
+2.1 Key Challenges
+  One of the key challenges faced in producing the data is to merge/join data across various files which does not contain the necessary header row. While the course project only requires the extraction of mean and standard deviation features, there is no explicit mention of which feature name to include/exclude. Assumptions are made along the way to produce the final result.
 
-You can also embed plots, for example:
+2.2 Assumptions Made
+* The respective columns in X_Train.txt and X_Test.txt are ordered with the sequence in Features.txt. E.g. we can assume the first column in X_Train refers to the first row in Features.txt
+* Only variable with mean() and std() are considered for the final tidy data
+* the Train and Test data could be merged without the need later to distinguish between Training and Testing data
+* the Inertia folder data is not required as the data in the training and test set are sufficient (after retaining only mean and std calculated fields)
 
-```{r, echo=FALSE}
-plot(cars)
-```
+2.3 Approach on processing the data
+2.3.1 Process main Data such as activity, feature
+2.3.2 Further process the feature data to extract only mean() and std()
+2.3.3 Process the Subject Data
+2.3.4 Process the Activty Data
+2.3.5 Process the measurements Data
+2.3.6 Column bind/Append all together
+2.3.7 Improve the description of the Activity with Activity labels
+2.3.8 Calculate the average to form the final tidy data
 
-Note that the `echo = FALSE` parameter was added to the code chunk to prevent printing of the R code that generated the plot.
+3. Variables Description
+
+3.1 Overall Naming Convention
+- ID_ refers to unique Identificatio number of the variable
+- NM_ refers to the label name
+- TIME_ refers to the Time component of the feature
+- FREQ_ refers to the Frequency component of the feature
+- _MEAN_ refers to the mean() calculation
+- _STD_ refers to the std() calculation
+
+3.1 ID_SUBJECT
+  This is the unique ID that identifies the 30 different volunteers that participated in the 
+
+3.2 ID_ACTIVTY
+  This is the unique ID for activities
+  
+3.3 NM_ACTIVITY
+  Contains the actual label of the activities which are mapped to the ID_ACTIVITY as follows:
+---  
+ID_ACTIVITY        NM_ACTIVITY
+---
+1                       WALKING
+2              WALKING_UPSTAIRS
+3            WALKING_DOWNSTAIRS
+4                       SITTING
+5                      STANDING
+6                        LAYING
+
+3.4 Remaining Features Columns
+  The feature columns are largely retained in its naming convention as of source (feature_info.txt). Only a slight modification to the variables name to make it more readable.
+  For example.
+  * t has been replaced by TIME_
+  * f has been replaced by FREQ_
+  * mean() has been replaced by _MEAN_
+  * std() has been replaced by _STD_
+
+4. Tidy Data Output:  Result.txt
+  The final output result produces the average of the features at each subject and activities grouping. Hence, the final output has 180 rows (30 volunteers by 6 different activities).
+
+*** End of Readme.md ****
+
+
--- run_analysis.R
+++ run_analysis.R
@@ -11,9 +11,105 @@ setwd("/Users/kilopy82/Coursera/Data Science Specialization/Getting and Cleaning
 
 library(dplyr)
 library(Hmisc)
-
+library(reshape2)
 # Approach
-# 1. Work on creating a single dataset for Train
-#    - Merge the Subject to the Training Set
-#    - Create a new variable called Category (refer to Cookbook on variable description)
-# 2. 
+# 1. Process Main Data such as activity, feature
+# 2. Further process the feature data to extract only mean() and std()
+# 3. Process the Subject Data
+# 4. Process the Activty Data
+# 5. Process the measurements Data
+# 6. Column Append all together
+# 7. Improve the description of the Activity with Label
+# 8. Calculate the average to form the tidy data
+
+## 1. Process Main Data
+## Extract the Main Labels for Activity and Feature Measured
+activity_label_File <- "./activity_labels.txt"
+activity_label_Data <- read.table(file=activity_label_File, header=FALSE, col.names = c("ID_ACTIVITY", "NM_ACTIVITY"))
+
+feature_label_File <- "./features.txt"
+feature_label_Data <- read.table(file=feature_label_File, header=FALSE, col.names = c("ID_FEATURE", "NM_FEATURE"))
+
+## 2. Further process the feature data to extract only mean() and std()
+# Use of Regular Expression to tidy the variable naming 
+
+feature_label_Data <- feature_label_Data %>% mutate(NM_FEATURE = sub("(^t)(+)" , "TIME_\\2", NM_FEATURE)) %>%
+  mutate(NM_FEATURE = sub("(^f)(+)" , "FREQ_\\2", NM_FEATURE)) %>%
+  mutate(NM_FEATURE = sub("(+)(-mean\\(\\)[-]*)(*)" , "\\1_MEAN_\\3", NM_FEATURE)) %>%
+  mutate(NM_FEATURE = sub("(+)(-std\\(\\)[-]*)(*)" , "\\1_STD_\\3", NM_FEATURE))                          
+
+# Assumption: Extract only features of its Mean or Standard Deviation calculation
+# Note that for mean, the label contains "mean()" 
+# Note that for standard deviation, the label contains "std()"
+
+# Extract the various index of the label
+label_Mean_FUN <- grep("_MEAN_" , feature_label_Data$NM_FEATURE) 
+label_Std_FUN <- grep("_STD_" , feature_label_Data$NM_FEATURE)
+
+# Perform the necessary processing to ensure the indices are unique and sorted
+require_Feature_list <- data.frame(ID_FEATURE = c(label_Mean_FUN,label_Std_FUN))
+require_Feature_list$ID_FEATURE <- unique(require_Feature_list$ID_FEATURE)
+require_Feature_list <- require_Feature_list[order(require_Feature_list$ID_FEATURE), ]
+
+# Retrieve all labels that are required for the exercise
+# requireFeatures_label <- feature_label_Data[require_Feature_list$ID_FEATURE, ]
+
+
+## 3. Process the Subject Data
+## Extract data from the train folder
+subject_Train_File <- "./train/subject_train.txt"
+subject_Train_Data <- read.table(file=subject_Train_File, header=FALSE, col.names = c("ID_SUBJECT"), colClasses = c("integer"))
+
+## Extract data from the test folder
+subject_Test_File <- "./test/subject_test.txt"
+subject_Test_Data <- read.table(file=subject_Test_File, header=FALSE, col.names = c("ID_SUBJECT"), colClasses = c("integer"))
+
+# Append the train and test subject data together
+subject_data <- rbind(subject_Train_Data, subject_Test_Data)
+
+
+## 4. Process the Activty Data
+## Extract data from the train folder
+activity_Train_File <- "./Train/y_train.txt"
+activity_Train_Data <- read.table(file=activity_Train_File, header=FALSE, col.names = c("ID_ACTIVITY"), colClasses = c("integer"))
+
+## Extract data from the test folder
+activity_Test_File <- "./Test/y_test.txt"
+activity_Test_Data <- read.table(file=activity_Test_File, header=FALSE, col.names = c("ID_ACTIVITY"), colClasses = c("integer"))
+
+activity_data <- rbind(activity_Train_Data, activity_Test_Data)
+
+
+## 5. Process the measurements Data
+## Extract data from the train folder
+measurement_Train_File <- "./train/X_train.txt"
+measurement_Train_Data <- read.table(file=measurement_Train_File, header=FALSE, col.names = feature_label_Data$NM_FEATURE, colClasses = c("numeric"))
+
+## Extract data from the test folder
+measurement_Test_File <- "./test/X_test.txt"
+measurement_Test_Data <- read.table(file=measurement_Test_File, header=FALSE, col.names = feature_label_Data$NM_FEATURE, colClasses = c("numeric"))
+
+measurement_data <- rbind(measurement_Train_Data, measurement_Test_Data)
+
+measurement_data_subset <- select(measurement_data , require_Feature_list)
+
+
+## 6. Column Append all together
+tidy_data <- cbind(subject_data, activity_data, measurement_data_subset)
+
+## 7. Improve the description of the Activity with Label
+
+tidy_data_final <- merge(y=tidy_data, x =activity_label_Data, by.x="ID_ACTIVITY" , by.y="ID_ACTIVITY" )
+
+## 8. Calculate the average to form the tidy data
+
+# Prepare the tidy data into subsets. Group by ID_SUBJECT and NM_ACTIVITY
+tidy_data_group <- select(tidy_data_final, ID_SUBJECT, NM_ACTIVITY)
+tidy_data_col <- select(tidy_data_final, -(ID_ACTIVITY:ID_SUBJECT)) 
+
+# Aggregate the data by ID_SUJBECT and NM_ACTIVITY. Sort the final output
+result <- aggregate(tidy_data_col, tidy_data_group, mean)
+result <- result[order(result$ID_SUBJECT) ,]
+
+# Output results to text file
+write.table(x=result, file="./result.txt", row.names = FALSE, sep=",")
